f9b646b584926cebdd129697c0e4317595187edc
Dawid Kańtoch <kantochdawid98@gmail.com>
2022-03-14 11:46:08 +0100

    pokazywanie ukrywanie menu z kontaktami

--- src/main/frontend-app/src/app/components/home/home.component.ts
+++ src/main/frontend-app/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isVisible = false;
 
-  constructor() { }
+  constructor(private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
 
+  showContacts() {
+    if(this.isVisible)
+    {
+      this.isVisible = false;
+      this.toastr.success("Schowano");
+    }
+    else {
+      this.isVisible = true;
+      this.toastr.success("Pokazano");
+    }
+
+  }
 }
